9f3a075c96ce0e7ac33e29099f4a03d1defb801e
AumBhatt <aum9698@outlook.com>
2021-08-30 02:14:04 +0400

    Setting up vue cli + basic numKeys added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Keys>{{key}}</Keys>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Keys from './components/Keys.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Keys
   }
 }
 </script>
---
+++ src/components/Keys.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="function-keys"></div>
+    <div
+        id="num-keys"
+        class="num-keys"
+    >
+        <button
+            v-for="key in numKeys"
+            :key="key"
+            class="active"
+            @click="printKey(key)"
+        >
+            {{ key }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Keys",
+    data() {
+        return {
+            numKeys: [
+                {id: 0, }
+            ]
+        }
+    },
+    methods: {
+        printKey(key) {
+            console.log(key)
+        }
+    }
+}
+</script>
+
+<style>
+
+.num-keys {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+}
+
+.active {
+}
+</style>
